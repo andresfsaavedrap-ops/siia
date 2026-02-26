74617d55a32ac5059ace84301386deda7dec41a2
felnandinho <f3lnandinho@felnandinho.saa>
2026-02-25 21:12:27 -0500

    fix: Corregir rutas de importación y actualizar dependencias
    - Corregir rutas de importación en index.js (./functions/ a ../functions/)
    - Agregar bootstrap como dependencia
    - Actualizar package-lock.json y yarn.lock

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -5,8 +5,8 @@ import { Tempo } from '@formkit/tempo';
 import 'datatables.net';
 
 // Importar tus funciones personalizadas
-import './functions/globals.js';
-import './functions/main.js';
+import '../functions/globals.js';
+import '../functions/main.js';
 // ... otros imports
 
 // Hacer jQuery disponible globalmente (para compatibilidad)
